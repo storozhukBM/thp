71a94c83014e03e5a1318101c882029189ff97c3
bohdanstorozhuk <storozhuk.b.m@gmail.com>
2022-11-01 14:53:33 +0000

    First working implementation

--- chan.go
+++ chan.go
@@ -2,45 +2,104 @@ package thp
 
 import (
 	"context"
+	"fmt"
+	"runtime"
 )
 
-type Chan[T any] struct{}
+type Chan[T any] struct {
+	batchSize    int
+	internalChan chan []T
+}
 
-func NewChan[T any]() *Chan[T] {
-	return &Chan[T]{}
+func NewChan[T any](batchSize int) (*Chan[T], func()) {
+	if batchSize < 1 {
+		panic("Batch size for thp.Chan can't be lower than 1")
+	}
+	ch := &Chan[T]{
+		batchSize:    batchSize,
+		internalChan: make(chan []T, 2*runtime.NumCPU()),
+	}
+	return ch, ch.Close
+}
+
+func (ch *Chan[T]) Close() {
+	close(ch.internalChan)
 }
 
 func (ch *Chan[T]) Producer(ctx context.Context) (*Producer[T], func()) {
-	result := &Producer[T]{ctx: ctx}
+	result := &Producer[T]{
+		parent: ch,
+		ctx:    ctx,
+		batch:  make([]T, 0, ch.batchSize),
+	}
 	return result, result.Flush
 }
 
 type Producer[T any] struct {
-	ctx context.Context
+	ctx    context.Context
+	parent *Chan[T]
+	batch  []T
 }
 
 func (p *Producer[T]) Flush() {
+	// TODO: write documentation on how to avoid items dropping
+	// in case of ctx cancelation using detached context
+	select {
+	case p.parent.internalChan <- p.batch:
+	case <-p.ctx.Done():
+		// we can't block this goroutine anymore
+		// and will drop batched items
+	}
+	fmt.Printf("batch flushed\n")
+
+	p.batch = make([]T, 0, p.parent.batchSize)
 }
 
 func (p *Producer[T]) Put(v T) {
-}
-
-func (ch *Chan[T]) Consumer(ctx context.Context) (*Consumer[T], func()) {
-	result := &Consumer[T]{ctx: ctx}
-	return result, result.ReturnUnconsumed
+	p.batch = append(p.batch, v)
+	if len(p.batch) >= p.parent.batchSize {
+		p.Flush()
+	}
 }
 
 type Consumer[T any] struct {
-	ctx context.Context
+	ctx    context.Context
+	parent *Chan[T]
+	idx    int
+	batch  []T
 }
 
-func (c *Consumer[T]) ReturnUnconsumed() {
-	// check buffered messages and schedule
-	// separate goroutine to re-submit them if some are unconsumed
+func (ch *Chan[T]) Consumer(ctx context.Context) *Consumer[T] {
+	result := &Consumer[T]{
+		ctx:    ctx,
+		parent: ch,
+		idx:    0,
+		batch:  nil,
+	}
+	return result
 }
 
-func (c *Consumer[T]) Poll() (T, bool) {
-	return zero[T](), false
+func (c *Consumer[T]) prefetch() bool {
+	c.idx = 0
+	select {
+	case batch, ok := <-c.parent.internalChan:
+		c.batch = batch
+		return ok
+	case <-c.ctx.Done():
+		return false
+	}
+}
+
+func (c *Consumer[T]) Poll() (value T, chAndCtxOpen bool) {
+	if c.idx >= len(c.batch) {
+		ok := c.prefetch()
+		if !ok {
+			return zero[T](), ok
+		}
+	}
+	item := c.batch[c.idx]
+	c.idx++
+	return item, true
 }
 
 func (c *Consumer[T]) NonBlockingPoll() (T, bool) {
--- chan_test.go
+++ chan_test.go
@@ -3,23 +3,56 @@ package thp_test
 import (
 	"context"
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"testing"
 
 	"github.com/storozhukBM/thp"
 )
 
-func TestChanPrimitives(t *testing.T) {
-	ch := thp.NewChan[int]()
-	consumer, consumerClose := ch.Consumer(context.Background())
-	defer consumerClose()
-	val, _ := consumer.Poll()
-	fmt.Printf("val: %T: `%+v`\n", val, val)
-}
+func TestChan(t *testing.T) {
+	ch, chCloser := thp.NewChan[int](32)
 
-func TestChanObj(t *testing.T) {
-	ch := thp.NewChan[*int]()
-	consumer, consumerClose := ch.Consumer(context.Background())
-	defer consumerClose()
-	val, _ := consumer.Poll()
-	fmt.Printf("val: %T: `%+v`\n", val, val)
+	producersCnt := 8
+	itemsPerProducer := 100
+	producersWg := &sync.WaitGroup{}
+	producersWg.Add(producersCnt)
+	for i := 0; i < producersCnt; i++ {
+		go func() {
+			defer producersWg.Done()
+			producer, flush := ch.Producer(context.Background())
+			defer flush()
+			for j := 0; j < itemsPerProducer; j++ {
+				producer.Put(1)
+			}
+		}()
+	}
+
+	consumersCnt := 8
+	consumersWg := &sync.WaitGroup{}
+	consumersWg.Add(consumersCnt)
+	counter := &atomic.Int64{}
+	for i := 0; i < consumersCnt; i++ {
+		go func() {
+			defer consumersWg.Done()
+			consumer := ch.Consumer(context.Background())
+			result := 0
+			item, ok := consumer.Poll()
+			for ; ok; item, ok = consumer.Poll() {
+				result += item
+			}
+			counter.Add(int64(result))
+		}()
+	}
+
+	producersWg.Wait()
+	chCloser()
+	fmt.Printf("Produsers finished\n")
+	consumersWg.Wait()
+	fmt.Printf("Consumers finished: %v\n", counter.Load())
+
+	expectedResult := int64(producersCnt * itemsPerProducer)
+	if counter.Load() != expectedResult {
+		t.Errorf("result is not as expected: %v != %v", counter.Load(), expectedResult)
+	}
 }

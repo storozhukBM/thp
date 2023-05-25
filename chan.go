5089b263d6e17fc8b91571507b0ed92b3f9c47ed
bohdanstorozhuk <storozhuk.b.m@gmail.com>
2023-05-25 22:08:57 +0100

    Use new version of thp channel as a primary implementation

--- chan.go
+++ chan.go
@@ -10,8 +10,14 @@ const ErrChanBatchSize chanError = "Batch size for thp.Chan can't be lower than 
 
 type Chan[T any] struct {
 	batchSize    int
-	internalChan chan []T
+	internalChan chan *batch[T]
 	batchPool    sync.Pool
+}
+
+// We use pointer to a batch instead of regular slice, to place it into sync.Pool.
+// This helps us to avoid extra allocation when storing it as interface{}/any.
+type batch[T any] struct {
+	buf []T
 }
 
 func NewChan[T any](batchSize int) (*Chan[T], func()) {
@@ -21,10 +27,12 @@ func NewChan[T any](batchSize int) (*Chan[T], func()) {
 
 	ch := &Chan[T]{
 		batchSize:    batchSize,
-		internalChan: make(chan []T, runtime.NumCPU()),
+		internalChan: make(chan *batch[T], runtime.NumCPU()),
 		batchPool: sync.Pool{
 			New: func() any {
-				return make([]T, 0, batchSize)
+				// We use pointer to a batch instead of regular slice, to place it into sync.Pool.
+				// This helps us to avoid extra allocation when storing it as interface{}/any.
+				return &batch[T]{buf: make([]T, 0, batchSize)}
 			},
 		},
 	}
@@ -36,42 +44,52 @@ func (ch *Chan[T]) Close() {
 	close(ch.internalChan)
 }
 
-func (ch *Chan[T]) getBatchFromPool() []T {
+// We use pointer to a batch instead of regular slice, to place it into sync.Pool.
+// This helps us to avoid extra allocation when storing it as interface{}/any.
+func (ch *Chan[T]) getBatchFromPool() *batch[T] {
 	//nolint:forcetypeassert // panic on type missmatch is fine here
-	return ch.batchPool.Get().([]T)
+	return ch.batchPool.Get().(*batch[T])
 }
 
-func (ch *Chan[T]) putBatchToPool(batch []T) {
-	batch = batch[:0]
-	//nolint:staticcheck // we will revisit potential allocation here later
+// We use pointer to a batch instead of regular slice, to place it into sync.Pool.
+// This helps us to avoid extra allocation when storing it as interface{}/any.
+func (ch *Chan[T]) putBatchToPool(batch *batch[T]) {
+	batch.buf = batch.buf[:0]
 	ch.batchPool.Put(batch)
 }
 
 func (ch *Chan[T]) Producer(ctx context.Context) (*Producer[T], func()) {
+	initialBatch := ch.getBatchFromPool()
 	result := &Producer[T]{
 		parent: ch,
 		ctx:    ctx,
-		batch:  ch.getBatchFromPool(),
+		batch:  initialBatch,
+		buf:    initialBatch.buf,
 	}
-
 	return result, result.Flush
 }
 
 type Producer[T any] struct {
 	ctx    context.Context
 	parent *Chan[T]
-	batch  []T
+	batch  *batch[T]
+	// We unpack the batch.buf into a separate field to avoid extra memory hop
+	// every time we access it, this yields significant speedup on our tests.
+	// But we still need to store pointer to a batch to avoid allocations when working with sync.Pool.
+	buf []T
 }
 
 func (p *Producer[T]) NonBlockingFlush() bool {
-	if len(p.batch) == 0 || p.ctx.Err() != nil {
+	if len(p.buf) == 0 || p.ctx.Err() != nil {
 		return false
 	}
 	// TODO: write documentation on how to avoid items dropping
-	// in case of ctx cancelation using detached context
+	// in case of ctx cancellation using detached context
+	p.batch.buf = p.buf
 	select {
 	case p.parent.internalChan <- p.batch:
 		p.batch = p.parent.getBatchFromPool()
+		p.buf = p.batch.buf
 		return true
 	default:
 		return false
@@ -79,33 +97,36 @@ func (p *Producer[T]) NonBlockingFlush() bool {
 }
 
 // Flush if you call Flush after producer context is canceled,
-// Flush won't block but it is possible that it will send data over the channel.
+// Flush won't block, but it is possible that it will send data over the channel.
 func (p *Producer[T]) Flush() {
-	if len(p.batch) == 0 || p.ctx.Err() != nil {
+	if len(p.buf) == 0 || p.ctx.Err() != nil {
 		return
 	}
 	// TODO: write documentation on how to avoid items dropping
-	// in case of ctx cancelation using detached context
+	// in case of ctx cancellation using detached context
+	p.batch.buf = p.buf
 	select {
 	case p.parent.internalChan <- p.batch:
 		p.batch = p.parent.getBatchFromPool()
+		p.buf = p.batch.buf
 	case <-p.ctx.Done():
 		// we can't block this goroutine anymore
 		// and will drop batched items
-		p.batch = nil
+		p.batch = &batch[T]{}
+		p.buf = nil
 	}
 }
 
 func (p *Producer[T]) Put(v T) {
-	p.batch = append(p.batch, v)
-	if len(p.batch) >= p.parent.batchSize {
+	p.buf = append(p.buf, v)
+	if len(p.buf) >= p.parent.batchSize {
 		p.Flush()
 	}
 }
 
 func (p *Producer[T]) NonBlockingPut(v T) bool {
-	p.batch = append(p.batch, v)
-	if len(p.batch) >= p.parent.batchSize {
+	p.buf = append(p.buf, v)
+	if len(p.buf) >= p.parent.batchSize {
 		flush := p.NonBlockingFlush()
 		return flush
 	}
@@ -116,7 +137,11 @@ type Consumer[T any] struct {
 	ctx    context.Context
 	parent *Chan[T]
 	idx    int
-	batch  []T
+	batch  *batch[T]
+	// We unpack the batch.buf into a separate field to avoid extra memory hop
+	// every time we access it, this yields significant speedup on our tests.
+	// But we still need to store pointer to a batch to avoid allocations when working with sync.Pool.
+	buf []T
 }
 
 func (ch *Chan[T]) Consumer(ctx context.Context) *Consumer[T] {
@@ -124,20 +149,25 @@ func (ch *Chan[T]) Consumer(ctx context.Context) *Consumer[T] {
 		ctx:    ctx,
 		parent: ch,
 		idx:    0,
-		batch:  nil,
+		batch:  &batch[T]{},
+		buf:    nil,
 	}
 	return result
 }
 
 func (c *Consumer[T]) prefetch() bool {
-	c.idx = 0
-	if len(c.batch) > 0 {
+	if cap(c.buf) > 0 {
 		c.parent.putBatchToPool(c.batch)
 	}
+	c.idx = 0
 	c.batch = nil
+	c.buf = nil
 	select {
 	case batch, ok := <-c.parent.internalChan:
 		c.batch = batch
+		if batch != nil {
+			c.buf = batch.buf
+		}
 		return ok
 	case <-c.ctx.Done():
 		return false
@@ -146,14 +176,18 @@ func (c *Consumer[T]) prefetch() bool {
 
 //nolint:nonamedreturns // here we usenamesreturns to documents meaning of two returned booleans
 func (c *Consumer[T]) nonBlockingPrefetch() (readSuccess bool, channelIsOpen bool) {
-	c.idx = 0
-	if len(c.batch) > 0 {
+	if cap(c.buf) > 0 {
 		c.parent.putBatchToPool(c.batch)
 	}
+	c.idx = 0
 	c.batch = nil
+	c.buf = nil
 	select {
 	case batch, ok := <-c.parent.internalChan:
 		c.batch = batch
+		if batch != nil {
+			c.buf = batch.buf
+		}
 		return ok, ok
 	default:
 		return false, true
@@ -161,26 +195,26 @@ func (c *Consumer[T]) nonBlockingPrefetch() (readSuccess bool, channelIsOpen boo
 }
 
 func (c *Consumer[T]) Poll() (T, bool) {
-	if c.idx >= len(c.batch) {
+	if c.idx >= len(c.buf) {
 		ok := c.prefetch()
 		if !ok {
 			return zero[T](), false
 		}
 	}
-	item := c.batch[c.idx]
+	item := c.buf[c.idx]
 	c.idx++
 	return item, true
 }
 
 //nolint:nonamedreturns // here we usenamesreturns to documents meaning of two last returned booleans
 func (c *Consumer[T]) NonBlockingPoll() (value T, readSuccess bool, channelIsOpen bool) {
-	if c.idx >= len(c.batch) {
+	if c.idx >= len(c.buf) {
 		success, open := c.nonBlockingPrefetch()
 		if !success {
 			return zero[T](), success, open
 		}
 	}
-	item := c.batch[c.idx]
+	item := c.buf[c.idx]
 	c.idx++
 	return item, true, true
 }
--- chan_bench_test.go
+++ chan_bench_test.go
@@ -8,22 +8,10 @@ import (
 	"testing"
 
 	"github.com/storozhukBM/thp"
-	"github.com/storozhukBM/thp/thp2"
-	"github.com/storozhukBM/thp/thp3"
 )
 
 func BenchmarkChanThroughput(b *testing.B) {
 	for pIdx := 1; pIdx < 32; pIdx *= 2 {
-		for bufSize := 1; bufSize <= 1024; bufSize *= 2 {
-			b.Run(fmt.Sprintf("type:%s;pCnt:%d;cCnt:%d;buf:%d", "thp3", pIdx, 8, bufSize), func(b *testing.B) {
-				runThp3Chan(b, pIdx, 8, bufSize)
-			})
-		}
-		for bufSize := 1; bufSize <= 1024; bufSize *= 2 {
-			b.Run(fmt.Sprintf("type:%s;pCnt:%d;cCnt:%d;buf:%d", "thp2", pIdx, 8, bufSize), func(b *testing.B) {
-				runThp2Chan(b, pIdx, 8, bufSize)
-			})
-		}
 		for bufSize := 1; bufSize <= 1024; bufSize *= 2 {
 			b.Run(fmt.Sprintf("type:%s;pCnt:%d;cCnt:%d;buf:%d", "standard", pIdx, 8, bufSize), func(b *testing.B) {
 				runStandardChan(b, pIdx, 8, bufSize)
@@ -93,112 +81,6 @@ func runThpChan(b *testing.B, producersCnt int, consumersCnt int, bufferSize int
 	canRun.Add(1)
 
 	ch, chCloser := thp.NewChan[int](bufferSize)
-
-	itemsPerProducer := b.N / producersCnt
-	producersWg := &sync.WaitGroup{}
-	producersWg.Add(producersCnt)
-	for i := 0; i < producersCnt; i++ {
-		go func() {
-			defer producersWg.Done()
-			producer, flush := ch.Producer(context.Background())
-			defer flush()
-			canRun.Wait()
-			for j := 0; j < itemsPerProducer; j++ {
-				producer.Put(1)
-			}
-		}()
-	}
-
-	consumersWg := &sync.WaitGroup{}
-	consumersWg.Add(consumersCnt)
-	counter := &atomic.Int64{}
-	for i := 0; i < consumersCnt; i++ {
-		go func() {
-			defer consumersWg.Done()
-			consumer := ch.Consumer(context.Background())
-			result := 0
-			canRun.Wait()
-			for item, ok := consumer.Poll(); ok; item, ok = consumer.Poll() {
-				result += item
-			}
-			counter.Add(int64(result))
-		}()
-	}
-
-	b.ResetTimer()
-	b.ReportAllocs()
-	canRun.Done()
-
-	producersWg.Wait()
-	chCloser()
-	consumersWg.Wait()
-	b.StopTimer()
-
-	expectedResult := int64(producersCnt * itemsPerProducer)
-	if counter.Load() != expectedResult {
-		b.Errorf("result is not as expected: %v != %v", counter.Load(), expectedResult)
-	}
-}
-
-//nolint:thelper // This is not exactly helper and in case of error we want to know line
-func runThp2Chan(b *testing.B, producersCnt int, consumersCnt int, bufferSize int) {
-	canRun := &sync.WaitGroup{}
-	canRun.Add(1)
-
-	ch, chCloser := thp2.NewChan[int](bufferSize)
-
-	itemsPerProducer := b.N / producersCnt
-	producersWg := &sync.WaitGroup{}
-	producersWg.Add(producersCnt)
-	for i := 0; i < producersCnt; i++ {
-		go func() {
-			defer producersWg.Done()
-			producer, flush := ch.Producer(context.Background())
-			defer flush()
-			canRun.Wait()
-			for j := 0; j < itemsPerProducer; j++ {
-				producer.Put(1)
-			}
-		}()
-	}
-
-	consumersWg := &sync.WaitGroup{}
-	consumersWg.Add(consumersCnt)
-	counter := &atomic.Int64{}
-	for i := 0; i < consumersCnt; i++ {
-		go func() {
-			defer consumersWg.Done()
-			consumer := ch.Consumer(context.Background())
-			result := 0
-			canRun.Wait()
-			for item, ok := consumer.Poll(); ok; item, ok = consumer.Poll() {
-				result += item
-			}
-			counter.Add(int64(result))
-		}()
-	}
-
-	b.ResetTimer()
-	b.ReportAllocs()
-	canRun.Done()
-
-	producersWg.Wait()
-	chCloser()
-	consumersWg.Wait()
-	b.StopTimer()
-
-	expectedResult := int64(producersCnt * itemsPerProducer)
-	if counter.Load() != expectedResult {
-		b.Errorf("result is not as expected: %v != %v", counter.Load(), expectedResult)
-	}
-}
-
-//nolint:thelper // This is not exactly helper and in case of error we want to know line
-func runThp3Chan(b *testing.B, producersCnt int, consumersCnt int, bufferSize int) {
-	canRun := &sync.WaitGroup{}
-	canRun.Add(1)
-
-	ch, chCloser := thp3.NewChan[int](bufferSize)
 
 	itemsPerProducer := b.N / producersCnt
 	producersWg := &sync.WaitGroup{}
